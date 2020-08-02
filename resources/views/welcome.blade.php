dfc9e946c56b2b60d62251615eb75752de2c24a0
LelliJacopo <lelli.jacopo@icloud.com>
2020-08-02 11:00:56 +0200

    Generate auth scaffolding & install npm packages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
               <div id="app"class="">
-                <h1>{{ message }}</h1>
+                
               </div>
                 <div class="title m-b-md">
                     Laravel

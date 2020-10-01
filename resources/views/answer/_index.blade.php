aaeaf934db10070f73fb1c3363254a0a21107c22
LelliJacopo <lelli.jacopo@icloud.com>
2020-10-01 08:24:18 +0200

    Added Update and Delete buttons

--- resources/views/answer/_index.blade.php
+++ resources/views/answer/_index.blade.php
@@ -27,18 +27,37 @@
   </div>
   <div class="media-body">
     {!! $answer->body_html !!}
-    <div class="float-right">
-      <span class="text-muted">
-        {{ $answer->created_date }}
-        <div class="media mt-2">
-          <a href="{{ $answer->user->url }}" class="pr-2">
-            <img src="{{ $answer->user->avatar  }}" alt="{{ $answer->user->name }}">
-          </a>
-          <div class="media-body mt-1">
-            <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
-          </div>
+    <div class="row">
+      <div class="col-4">
+        <div class="ml-auto ">
+          @can('update', $answer)
+          <a href="{{ route('question.answer.edit', [$question, $answer]) }}" class="btn btn-sm btn-outline-info my-2">{{ __('Edit') }}</a>
+          @endcan
+          @can('delete', $answer)
+          <form class="form-delete my-2" action="{{ route('question.answer.destroy', [$question, $answer]) }}" method="post">
+            @csrf
+            @method('DELETE')
+            <button type="submti" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">{{ __('Delete') }}</button>
+          </form>
+          @endcan
         </div>
-      </span>
+      </div>
+      <div class="col-4">
+
+      </div>
+      <div class="col-4">
+        <span class="text-muted">
+          {{ $answer->created_date }}
+          <div class="media mt-2">
+            <a href="{{ $answer->user->url }}" class="pr-2">
+              <img src="{{ $answer->user->avatar  }}" alt="{{ $answer->user->name }}">
+            </a>
+            <div class="media-body mt-1">
+              <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
+            </div>
+          </div>
+        </span>
+      </div>
     </div>
   </div>
 </div>

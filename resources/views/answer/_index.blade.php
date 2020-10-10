eb6628351d1ba514e80403a7d7395d3019416fd1
LelliJacopo <lelli.jacopo@icloud.com>
2020-10-10 17:20:08 +0200

    Tidy up the views

--- resources/views/answer/_index.blade.php
+++ resources/views/answer/_index.blade.php
@@ -1,34 +1,19 @@
-<div class="media">
-  @include('shared._vote', [
-    'model' => $answer
-  ])
-  <div class="media-body">
-    {!! $answer->body_html !!}
-    <div class="row">
-      <div class="col-4">
-        <div class="ml-auto ">
-          @can('update', $answer)
-          <a href="{{ route('question.answer.edit', [$question, $answer]) }}" class="btn btn-sm btn-outline-info my-2">{{ __('Edit') }}</a>
-          @endcan
-          @can('delete', $answer)
-          <form class="form-delete my-2" action="{{ route('question.answer.destroy', [$question, $answer]) }}" method="post">
-            @csrf
-            @method('DELETE')
-            <button type="submti" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">{{ __('Delete') }}</button>
-          </form>
-          @endcan
-        </div>
-      </div>
-      <div class="col-4">
+@if($question->answers_count > 0)
+  <div class="row mt-5 justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-body">
+          <div class="card-title">
+            <h2>{{ $question->answers_count." ".str_plural('Answer', $question->answers_count) }}</h2>
+          </div>
+          <hr>
+          @include('layouts._messages')
 
-      </div>
-      <div class="col-4">
-        @include('shared._author', [
-          'model' => $answer,
-          'label' => 'Answered'
-        ])
+          @foreach($question->answers as $answer)
+            @include('answer._answer')
+          @endforeach
+        </div>
       </div>
     </div>
   </div>
-</div>
-<hr>
+@endif
